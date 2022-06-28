247badb6fbeca5ee2b2bda2b88c7e5e1ce0582b8
Mr-Cruff <Chrishonne.Williams@gmail.com>
2022-06-28 16:27:58 -0500

    Initial commit

---
+++ src/main/java/com/jabgl/jbgl_api/JbglApiApplication.java
@@ -0,0 +1,13 @@
+package com.jabgl.jbgl_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JbglApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JbglApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jabgl/jbgl_api/JbglApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jabgl.jbgl_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JbglApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
